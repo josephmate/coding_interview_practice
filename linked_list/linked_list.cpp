48bcb1684b7a19d5ddd4ec3800709b06ff4cd1ad
Joseph <mate.joseph@gmail.com>
2016-01-22 00:24:48 -0500

    completed the linked_list impl along with test cases

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -34,12 +34,14 @@ int linked_list::get(int idx) {
 	if(idx < 0) {
 		throw  std::runtime_error("the idx must be >= 0");
 	}
-	int howManyToGoBack = currentSize - idx + 1;
+	// figure out how many elements we need to traverse to get to the one at the index
+	// recall that the first point is the item with the highest index
+	int howManyToGoBack = (currentSize-1) - idx;
 	if(howManyToGoBack < 0) {
 		throw  std::runtime_error("the idx must be < currentSize");
 	}
 	link_node * curr = tail;
-	for(int i = 0; i <= howManyToGoBack; i++) {
+	for(int i = 1; i <= howManyToGoBack; i++) {
 		curr = curr->next;
 	}
 
@@ -47,27 +49,56 @@ int linked_list::get(int idx) {
 }
 
 int linked_list::pop() {
+	if(currentSize <= 0) {
+		throw std::runtime_error("cannot pop from an empty list");
+	}
 	int data = tail->data;
 	link_node * to_delete = tail;
 	tail = tail->next;
+	currentSize--;
 	delete to_delete;
+	return data;
 }
 
 int linked_list::remove(int idx) {
-	if(idx < 0) {
-		throw std::runtime_error("the idx must be >= 0");
+	if(currentSize <= 0) {
+		throw std::runtime_error("cannot remove from an empty list");
 	}
-	int howManyToGoBack = currentSize - idx + 1;
+	if(idx < 0) {
+		throw  std::runtime_error("the idx must be >= 0");
+	}
+
+	// figure out how many elements we need to traverse to get to the one at the index
+	// recall that the first point is the item with the highest index
+	int howManyToGoBack = (currentSize-1) - idx;
 	if(howManyToGoBack < 0) {
-		throw std::runtime_error("the idx must be < currentSize");
+		throw  std::runtime_error("the idx must be < currentSize");
 	}
 	link_node * curr = tail;
-	for(int i = 0; i <= howManyToGoBack; i++) {
+	link_node * prev = NULL;
+	for(int i = 1; i <= howManyToGoBack; i++) {
+		// we need to keep track of the previous pointer because we link the
+		// previous and the next of current in order to remove the current:
+		//
+		//
+		// BEFORE  TAIL  ---->   TAIL-1 ----> ..... ----> PREV ----> CURR ----> CURR->NEXT
+		// AFTER                                             \________________> CURR->NEXT
+		//
+		prev = curr;
 		curr = curr->next;
 	}
-	// TODO
+	if(prev != NULL) {
+		prev->next=curr->next;
+	} else {
+		tail = curr->next;
+	}
+	int data = curr->data;
+	delete curr;
+	currentSize--;
+	return data;
 }
 
 int linked_list::size() {
+	// we keep track of changes to the size as the methods are accessed
 	return currentSize;
 }

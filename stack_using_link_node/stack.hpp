c4ddc45a6ff860a3b46407cc6f2f2cc73b9476a0
Joseph <mate.joseph@gmail.com>
2016-01-25 13:49:51 -0500

    completed stack and testing

--- stack_using_link_node/stack.hpp
+++ stack_using_link_node/stack.hpp
@@ -1,4 +1,7 @@
 
+#include <cstddef>
+#include <stdexcept>
+
 template <class T>
 struct link_node {
 	link_node<T> * next;
@@ -10,6 +13,13 @@ template <class T>
 class stack {
 
 	int currentSize;
+	/**
+	 *
+	 *  ln     --> ln     --> ln
+	 *  * ----/    * ----/    * : NULL
+	 *  1          2          3
+	 *
+	 */
 	link_node<T> * tail;
 
 	public:
@@ -31,4 +41,52 @@ class stack {
 		 */
 		int size();
 };
+
+
+template <class T>
+link_node<T>::link_node(link_node<T>* n, T d) {
+	next = n;
+	data = d;
+}
+
+template <class T>
+stack<T>::stack() {
+	currentSize = 0;
+	tail = NULL;
+}
+
+template <class T>
+stack<T>::~stack() {
+	while(tail != NULL) {
+		pop();
+	}
+}
+
+template <class T>
+void stack<T>::push(T v) {
+	link_node<T> * new_node = new link_node<T>(tail, v);
+	tail = new_node;
+	currentSize++;
+}
+
+template <class T>
+T stack<T>::pop() {
+	if(currentSize <= 0) {
+		throw std::runtime_error("cannot pop from an empty list");
+	}
+	T ret = tail->data;
+
+	link_node<T> * to_delete = tail;
+	tail = tail->next;
+	delete to_delete;
+
+	currentSize--;
+
+	return ret;
+}
+
+template <class T>
+int stack<T>::size() {
+	return currentSize;
+}
 

6537b6387d537c14cfbc5558215fb580e1ba1278
Joseph <mate.joseph@gmail.com>
2016-01-29 15:57:19 -0500

    implementation, main, and testing complete, bugfix for runjar.sh (unix newline issue)

--- remove_chars/src/main/java/RemoveChars.java
+++ remove_chars/src/main/java/RemoveChars.java
@@ -6,7 +6,20 @@ public class RemoveChars {
 	}
 
 	public static String removeChars(String input, String charsToRemove) {
-		return "";
+		Set<Character> removeSet = new HashSet<>();
+		for(int i = 0; i < charsToRemove.length(); i++) {
+			removeSet.add(charsToRemove.charAt(i));
+		}
+
+		StringBuilder strBuilder = new StringBuilder();
+		for(int i = 0; i < input.length(); i++) {
+			char curr = input.charAt(i);
+			if(!removeSet.contains(curr)) {
+				strBuilder.append(curr);
+			}
+		}
+
+		return strBuilder.toString();
 	}
 
 }
--- remove_chars/src/test/java/TestRemoveChars.java
+++ remove_chars/src/test/java/TestRemoveChars.java
@@ -4,7 +4,30 @@ import static org.junit.Assert.*;
 public class TestRemoveChars {
 
 	@Test
-	public void test() {
+	public void emtpyCases() {
+		// empty input empty remove 
+		assertEquals("", RemoveChars.removeChars("",""));
+		
+		// nonempty input empty remove
+		assertEquals("abc", RemoveChars.removeChars("abc",""));
+
+		// empty input nonempty remove 
+		assertEquals("", RemoveChars.removeChars("","abc"));
+	}
+
+	@Test
+	public void edgeCases() {
+		// removes nothing
+		assertEquals("abc", RemoveChars.removeChars("abc","efg"));
+		
+		// removes everything
+		assertEquals("", RemoveChars.removeChars("abc","abc"));
+		
+		// removes contains duplicates
+		assertEquals("cd", RemoveChars.removeChars("abcd","bbaa"));
+		
+		// input contains duplicates
+		assertEquals("aaccac", RemoveChars.removeChars("aabccddbdac","bd"));
 	}
 
 }

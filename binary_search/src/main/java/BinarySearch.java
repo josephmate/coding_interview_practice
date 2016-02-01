3b6cb163f5918e79d3947b2c63581c515b6017e1
Joseph <mate.joseph@gmail.com>
2016-02-01 02:56:45 -0500

    initialized the binary search solution

---
+++ binary_search/src/main/java/BinarySearch.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class BinarySearch {
+	
+	public BinarySearch() {
+	}
+
+
+	/**
+	 * array: the array to search in
+	 * lower: the lowest index to search from
+	 * upper: the highest index to search to
+	 * target: the value being search for in the array
+	 *
+	 * Returns the index that the target was found in. Otherwise, this method
+	 * returns -1.
+	 */
+	public static int binarySearch(int[] array, int lower, int upper, int target) {
+		return -1;
+	}
+
+
+}
---
+++ binary_search/src/test/java/TestBinarySearch.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestBinarySearch {
+
+	@Test
+	public void testHeap() {
+	}
+
+}

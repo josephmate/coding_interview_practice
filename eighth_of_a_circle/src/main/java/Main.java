929030edda59f2f93fb7af956a712ad55528777e
Joseph <mate.joseph@gmail.com>
2016-02-16 14:50:00 -0500

    drawing primitive ready

--- eighth_of_a_circle/src/main/java/Main.java
+++ eighth_of_a_circle/src/main/java/Main.java
@@ -4,6 +4,11 @@ import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.paint.Color;
 
+import javafx.scene.*;
+import javafx.scene.paint.*;
+import javafx.scene.canvas.*;
+
+
 public class Main extends Application {
 	
 
@@ -12,6 +17,9 @@ public class Main extends Application {
 
 	}
 	
+	private static final int HEIGHT = 300;
+	private static final int WIDTH = 300;
+	private GraphicsContext gc;
 	@Override
 	public void start(Stage primaryStage) {
 		List<String> args =getParameters().getRaw();
@@ -22,18 +30,45 @@ public class Main extends Application {
 		int argCounter = 0;
 		int radius = Integer.parseInt(args.get(argCounter++));
 		
-		canvas.getGraphicsContext2D().setFill(Color.White);
-		canvas.getGraphicsContext2D().fillRect(0, 0, 1, 1);
 
+		Group root = new Group();
+		Scene s = new Scene(root, WIDTH, HEIGHT, Color.BLACK);
+
+		final Canvas canvas = new Canvas(WIDTH, HEIGHT);
+		gc = canvas.getGraphicsContext2D();
+		 
+		gc.setFill(Color.WHITE);
+		gc.fillRect(0, 0, WIDTH, HEIGHT);
+		gc.setFill(Color.BLACK);
+		gc.strokeLine(WIDTH/2, 0, WIDTH/2, HEIGHT);
+		gc.strokeLine(0, HEIGHT/2, WIDTH, HEIGHT/2);
+		 
+		root.getChildren().add(canvas);
+		
+		drawEighthOfCircle(radius);
+
+		primaryStage.setScene(s);
 		primaryStage.show();
 	}
 
-
+	/**
+	 * x = 0, y = 0, is the middle
+	 */
   private void setPixel(int x, int y){
-		canvas.getGraphicsContext2D().getPixelWriter().setColor(x, y,Color.Black);
+		gc.getPixelWriter().setColor(WIDTH/2 + x, HEIGHT/2 - y, Color.BLACK);
 	}
 
 	private void drawEighthOfCircle( int radius ){
+		//TODO implement
+		setPixel(radius, radius);
+		setPixel(radius-1, radius);
+		for(int i = 1; i <= 10; i++) {
+			setPixel(radius+i, radius);
+		}
+		setPixel(radius, radius-1);
+		for(int i = 1; i <= 5; i++) {
+			setPixel(radius, radius+i);
+		}
 	}
 
 }
